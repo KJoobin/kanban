d512811d709ba0ef6bfd6a3b519fa0afc9c50319
kjoobin <kjoobin>
2022-02-08 09:23:59 +0900

    add lint & fix lint

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 export type IButton = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
+React.ButtonHTMLAttributes<HTMLButtonElement>,
+HTMLButtonElement
 >;
 
 export const Button: React.FC<IButton> = ({
