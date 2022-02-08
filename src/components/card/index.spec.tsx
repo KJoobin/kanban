54c7ba7f8b5734e0629b57a18921892eaa819af4
kjoobin <kjoobin>
2022-02-08 17:50:18 +0900

    fix type error in test file

--- src/components/card/index.spec.tsx
+++ src/components/card/index.spec.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@test";
-import { Card, CardStatus } from "./index";
-import { expect } from "@jest/globals";
+import { Card } from "./index";
+import { TaskStatus } from "@recoil/atoms";
+import { describe, expect, it } from "@jest/globals";
+
 
 describe("카드 컴포넌트", () => {
   it("정상적으로 렌더가 된다", () => {
-    render(<Card status={CardStatus.BACKLOG} />);
+    render(<Card status={TaskStatus.BACKLOG} />);
 
     expect(screen).toBeTruthy();
   });
 
   it("title 과, description 이 정상적으로 렌더가 된다", async () => {
-    render(<Card status={CardStatus.BACKLOG} title={"할일1"} description={"해야하는일 1"} />);
+    render(<Card status={TaskStatus.BACKLOG} title={"할일1"} description={"해야하는일 1"} />);
 
     const title = await screen.findByText("할일1");
     expect(title).toBeTruthy();
--- src/utils/deep-copy/index.spec.ts
+++ src/utils/deep-copy/index.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "@jest/globals";
 import { deepCopy } from "./index";
 
 describe("deep-copy", () => {
