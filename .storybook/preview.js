363e0a4c019a9ea1f8ed69c17920c2fb692ce8a4
kjoobin <kjoobin>
2022-02-08 21:58:47 +0900

    add root recoil in storybook settings decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import "../src/styles/global.css";
+import { RecoilRoot } from "recoil";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+export const decorators = [
+  Story => (
+    <RecoilRoot>
+      <Story />
+    </RecoilRoot>
+  )
+]
